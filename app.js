6ee0d1ba086ea599d1bff6e0751c0c25efa640c9
repl.it user <replituser@example.com>
2021-01-06 18:20:33 +0000

    Added the next() call inside the middleware. Fixes console log bug

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const express= require('express');
 
 const app = express();
 
-const server = http.createServer();
+app.use((req, res, next) => {
+  console.log('In the middleware');
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
