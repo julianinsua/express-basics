5818083a59113bc1bec63024333d119f9e721397
Julian <jii.julious@gmail.com>
2021-02-14 22:25:39 -0300

    reset of the course project to chapter 6

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const http = require('http');
-const express= require('express');
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware');
-});
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log('In another middleware');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
