e6a85d18b5440d7e37140c33eef71fe5a47b0dbe
julianinsua <jii.julious@gmail.com>
2021-03-20 14:10:15 -0300

    Created the functionality for jade, handlebars and EJS. The las one is implemented

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use('/admin', adminData.routes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render("404", {
+    pageTitle: "Page Not Found",
+    path: "random",
+  }) /*sendFile(path.join(__dirname, "views", "404.html"))*/;
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -9,8 +9,15 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const { products } = adminData;
+  res.render("shop", {
+    products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productsCSS: true,
+  });
 });
 
 module.exports = router;
