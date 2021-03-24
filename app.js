6cfef35cc23dca0dbb02ef90ae31a9892ce29921
julianinsua <jii.julious@gmail.com>
2021-03-23 21:57:42 -0300

    finished module 8

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
 const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
-
-const app = express();
-
-app.set("view engine", "ejs");
-app.set("views", "views");
-
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const { pageNotFound } = require("./controllers/404");
+const app = express();
+app.set("view engine", "ejs");
+
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", {
-    pageTitle: "Page Not Found",
-    path: "random",
-  }) /*sendFile(path.join(__dirname, "views", "404.html"))*/;
-});
+app.use(pageNotFound);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,23 +1,8 @@
-const path = require("path");
-
 const express = require("express");
-
-const rootDir = require("../util/path");
-
-const adminData = require("./admin");
+const productsControler = require("../controllers/products");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  const { products } = adminData;
-  res.render("shop", {
-    products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productsCSS: true,
-  });
-});
+router.get("/", productsControler.getProducts);
 
 module.exports = router;
