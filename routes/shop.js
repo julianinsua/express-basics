fdc8c127e180ee3f22beb104c1e7f08a73955bbd
julianinsua <jii.julious@gmail.com>
2021-07-09 23:45:05 -0300

    module 12 - MongoDB class 20

--- routes/shop.js
+++ routes/shop.js
@@ -18,15 +18,15 @@ router.get("/", getIndex);
 router.get("/products", getProducts);
 
 router.get("/products/:productId", getProductDetails);
-
-router.post("/cart", postCart);
-
-router.get("/cart", getCart);
-
-router.post("/cart-delete-item", postDeleteCartItem);
-
-router.get("/orders", getOrders);
-
-router.post("/create-order", postOrder);
+//
+// router.post("/cart", postCart);
+//
+// router.get("/cart", getCart);
+//
+// router.post("/cart-delete-item", postDeleteCartItem);
+//
+// router.get("/orders", getOrders);
+//
+// router.post("/create-order", postOrder);
 
 module.exports = router;
