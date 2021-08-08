167b6f1a1b220ef3a9dd49f32611d24782b8377d
julianinsua <jii.julious@gmail.com>
2021-08-07 21:25:50 -0300

    Module 14 (#3)
    * module 14 - class 3
    
    * module 14 - class 4
    
    * module 14 - class 8: Implemented Cookies
    
    * module 14 - class 8: Implemented Cookies
    
    * module 14 - class 8: Implemented Cookies
    
    * module 14 - class 10: Initialized session middleware
    
    * module 14 - class 11: implemented session logic to store authentication status
    
    * module 14 - class 16: implemented logout
    
    * module 14 - class 17: fixed user from session and conditional rendering
    
    * module 14 - finished Module

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
+const SessionStore = require("connect-mongodb-session")(session);
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const User = require("./models/user");
@@ -8,24 +10,44 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
+const store = new SessionStore({
+  uri: process.env.API_URL,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
 app.use((req, res, next) => {
-  User.findById("6104165d48e03fc6ff55d477")
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((e) => console.log(e));
+  if (req.session.user) {
+    User.findById(req.session.user._id)
+      .then((user) => {
+        req.user = user;
+        next();
+      })
+      .catch((e) => console.log(e));
+  } else {
+    next();
+  }
 });
+
+app.use(authRoutes);
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
