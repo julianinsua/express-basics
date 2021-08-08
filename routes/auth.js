a21c1283ed3b8780b71ae16999e3e5f40c99e141
Julian <jii.julious@gmail.com>
2021-08-08 13:03:39 -0300

    module 15 - Class 4: updated views to support sign up

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { getLogin, postLogin, postLogout } = require("../controllers/auth");
+const {
+  getLogin,
+  postLogin,
+  postLogout,
+  getSignup,
+} = require("../controllers/auth");
 
 router.get("/login", getLogin);
 router.post("/login", postLogin);
 router.post("/logout", postLogout);
+router.get("/signup", getSignup);
 
 module.exports = router;
