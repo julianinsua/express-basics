c8c00e573152f3e80d07a95f171e2539feac6845
Julian <jii.julious@gmail.com>
2021-09-07 19:00:20 -0300

    module 23 - Finished module: Accepting payments

--- routes/shop.js
+++ routes/shop.js
@@ -11,6 +11,7 @@ const {
   postDeleteCartItem,
   postOrder,
   getInvoice,
+  getCheckoutSuccess,
 } = require("../controllers/shop");
 
 const router = Router();
@@ -27,9 +28,13 @@ router.get("/cart", isAuth, getCart);
 
 router.post("/cart-delete-item", isAuth, postDeleteCartItem);
 
-router.get("/orders", isAuth, getOrders);
+router.get("/checkout", isAuth, getCheckout);
 
-router.post("/create-order", isAuth, postOrder);
+router.get("/checkout/success", isAuth, getCheckoutSuccess);
+
+router.get("/checkout/cancel", isAuth, getCheckout);
+
+router.get("/orders", isAuth, getOrders);
 
 router.get("/orders/:orderId", isAuth, getInvoice);
 
